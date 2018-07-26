89828b35afa0c561225fad8db8f48a91abc10042
vikash verma <vikashraj144@gmail.com>
2018-07-26 15:28:12 +0530

    login compoent added

--- Angular6/src/app/shared/components/login/login.component.ts
+++ Angular6/src/app/shared/components/login/login.component.ts
@@ -1,3 +1,6 @@
+import { RestService } from './../../rest.service';
+import { UserService } from './../../service/user/user.service';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginform: FormGroup;
+  username: FormControl;
+  password: FormControl;
 
-  constructor() { }
+
+  userdata: any;
+  errorMessage = '';
+
+  constructor(private _userService: UserService, private restWrapperService: RestService) { }
+
 
   ngOnInit() {
+    this.createForm();
+
   }
 
+  createForm() {
+    this.loginform = new FormGroup({
+      username: this.username,
+      password: this.password
+    });
+  }
+
+  onSubmit() {
+    const loginFormData = {
+      username: this.loginform.controls.username.value,
+      password: this.loginform.controls.password.value
+    };
+    this._userService.userLogin(loginFormData).subscribe(
+      (rsp: any) => {
+        if (rsp) {
+          console.log(rsp, 'response after final step');
+          this.userdata = rsp;
+        }
+      },
+      error => this.errorMessage = 'An error was encountered loading your order history.'
+    );
+  }
 }
