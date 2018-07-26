bb22154db5b6eccd3299f35ca45ffb4b53691488
vikash verma <vikashraj144@gmail.com>
2018-07-26 16:44:49 +0530

    register comp

--- Angular6/src/app/shared/components/register/register.component.ts
+++ Angular6/src/app/shared/components/register/register.component.ts
@@ -1,3 +1,6 @@
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { RestService } from './../../rest.service';
+import { UserService } from './../../service/user/user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  loginform: FormGroup;
+  username: FormControl;
+  password: FormControl;
 
-  constructor() { }
+  constructor(private _userService: UserService, private restWrapperService: RestService) { }
 
   ngOnInit() {
+    this.createFormControls();
+    this.createForm();
   }
 
+  createForm() {
+    this.loginform = new FormGroup({
+      username: this.username,
+      password: this.password
+    });
+  }
+
+  createFormControls() {
+    this.username = new FormControl('', [
+      Validators.required,
+      Validators.pattern('[^ @]*@[^ @]*')
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ]);
+  }
+
+  onRegister() {
+    console.log('register');
+  }
 }
