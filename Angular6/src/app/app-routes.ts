1be658a8f9ba8f814dad3ac7d5195cba612cdd98
vikash verma <vikashraj144@gmail.com>
2018-07-30 15:19:12 +0530

    adder route

--- Angular6/src/app/app-routes.ts
+++ Angular6/src/app/app-routes.ts
@@ -1,7 +1,14 @@
+import { LoginComponent } from './shared/components/login/login.component';
+import { AlwaysAuthGuard } from './shared/service/guard/always-auth-guard';
+import { OnlyLoggedInUsersGuard } from './shared/service/guard/only-loggedIn-users-guard';
+
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 export const appRoutes: Routes = [
     { path: '', component: AppComponent, pathMatch: 'full' },
-    { path: '**', redirectTo: '', pathMatch: 'full' }
+    { path: 'login', component: LoginComponent },
+    { path: 'dashboard', component: DashboardComponent, canActivate: [OnlyLoggedInUsersGuard, AlwaysAuthGuard] },
+    { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
