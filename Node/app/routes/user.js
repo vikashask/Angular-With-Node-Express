75ca43b0bda8fecb73815dcc7c04d8eb4d4e5733
vikash verma <vikashraj144@gmail.com>
2018-07-30 17:57:22 +0530

    login api changed

--- Node/app/routes/user.js
+++ Node/app/routes/user.js
@@ -39,9 +39,23 @@ function postUser(req, res) {
  * POST /book to save a new book.
  */
 function login(req, res) {
-console.log(req.body,'login data---');
     User.find({email:req.body.email,password:req.body.password},(function(error,data){
-        res.json(data);
+        if(data){
+            const loginDetails = {
+                data:data,
+                msg:'login sussessfull',
+                isLogin:true
+            }
+            console.log(loginDetails);
+            res.json(data);
+        }else{
+            const loginDetails = {
+                data:data,
+                msg:'Invalid details',
+                isLogin:false
+            }
+            res.json(data)
+        }
     }));
 }
 
